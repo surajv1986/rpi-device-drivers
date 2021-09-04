fea7f1185fa707110e0427be62af5e2d4ee417b0
surajv1986@gmail.com <surajv1986@gmail.com>
2021-09-04 14:18:31 +0530

    Initial commit

---
+++ driver.c
@@ -0,0 +1,134 @@
+/*
+ *
+ *
+ * */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+/* Memory Size */
+#define mem_size 1024
+
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+uint8_t *kernel_buffer;
+
+/* Function prototypes */
+static int __init etx_driver_init(void);
+static void __exit etx_driver_exit(void);
+static int etx_open(struct inode *inode, struct file *file);
+static int etx_release(struct inode *inode, struct file *file);
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t etx_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+
+/* File operations structure */
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = etx_read,
+	.write = etx_write,
+	.open = etx_open,
+	.release = etx_release,
+};
+
+static int etx_open(struct inode *inode, struct file *file)
+{
+	/* Creating physical memory */
+	if ((kernel_buffer = kmalloc(mem_size, GFP_KERNEL)) == 0) {
+		pr_info("Cannot allocate memory in kernel\n");
+		return -1;
+	}
+	pr_info("Device file opened\n");
+	return 0;
+
+}
+
+static int etx_release(struct inode *inode, struct file *file)
+{
+	kfree(kernel_buffer);
+	pr_info("Device File closed...\n");
+	return 0;
+}
+
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+	if (copy_to_user(kernel_buffer, buf, len)) {
+		pr_err("Data read error \n");
+	}
+	pr_info("Data read: Done!\n");
+
+	return mem_size;
+
+}
+
+static ssize_t etx_write(struct file *filp, const char *buf, size_t len, loff_t *off)
+{
+	if (copy_from_user(kernel_buffer, buf, len)) {
+		pr_err("Data write: Error\n");
+	}
+	pr_info("Data write : Done");
+
+	return len;
+}
+
+static int __init etx_driver_init(void)
+{
+	if ((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) < 0 ) {
+		pr_info("Cannot allocate major number\n");
+		return -1;
+	}
+
+	pr_info("Major = %d Minor = %d\n", MAJOR(dev), MINOR(dev));
+	
+	/* Creating cdev structure */
+	cdev_init(&etx_cdev, &fops);
+
+	/* Adding character device to the system */
+	if ((dev_class = class_create(THIS_MODULE, "etx_class")) == NULL) {
+		pr_info("Cannot create the struct class\n");
+		goto r_class;
+	}
+
+	/* Creating device */
+	if ((device_create(dev_class, NULL, dev, NULL, "etx_device")) == NULL) {
+		pr_info("Cannot create the Device 1 \n");
+		goto r_device;	
+	}
+
+	pr_info("Device Driver Insert... Done!!!\n");
+
+	return 0;
+
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev, 1);
+
+	return -1;
+
+}
+
+static void __exit etx_driver_exit(void)
+{
+	device_destroy(dev_class, dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev, 1);
+	pr_info("Device Driver Remove...Done!!!\n");
+}
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EmbeTronicx<embetronicx@gmail.com>");
+MODULE_DESCRIPTION("Simple Linux Device Driver for rpi  \n");
+MODULE_VERSION("1.4");
+
---
+++ driver.mod.c
@@ -0,0 +1,51 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0xa11440c8, "module_layout" },
+	{ 0x7a39daf9, "cdev_del" },
+	{ 0x9dd4af8d, "device_destroy" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x5b6dfd24, "class_destroy" },
+	{ 0x4060a0c4, "device_create" },
+	{ 0x49d2824e, "__class_create" },
+	{ 0x1eacc803, "cdev_init" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x51a910c0, "arm_copy_to_user" },
+	{ 0x2cfde9a2, "warn_slowpath_fmt" },
+	{ 0x5f754e5a, "memset" },
+	{ 0xae353d77, "arm_copy_from_user" },
+	{ 0xbed00335, "kmem_cache_alloc_trace" },
+	{ 0x55f8e9c0, "kmalloc_caches" },
+	{ 0xc5850110, "printk" },
+	{ 0x37a0cba, "kfree" },
+	{ 0xb1ad28e0, "__gnu_mcount_nc" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "3DE46DC3D55C28BCB105B0B");
